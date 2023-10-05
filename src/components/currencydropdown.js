efa44fa406ddca91a04caeb76432911a4c60e3c0
KuforAdrian <adrianteintsem@gmail.com>
2023-10-05 01:06:28 -0400

    dispatch currency symbol to the context API

--- src/components/currencydropdown.js
+++ src/components/currencydropdown.js
@@ -1,19 +1,31 @@
-//import React, { useContext, useState } from 'react';
+import React, { useContext} from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import './componentcss/style.css';
+import { AppContext } from '../context/AppContext';
 
 function CurrencyDropDown() {
-  return (
-    <Dropdown className="drop">
+   
+    const { dispatch } = useContext(AppContext);
+
+
+    const selectedCurrency = (event) => {
+            dispatch({
+                type: 'CHG_CURRENCY',
+                payload: event
+            })
+     }
+
+   return (
+    <Dropdown onSelect={selectedCurrency} className="drop">
       <Dropdown.Toggle variant="success" id="dropdown-basic" >
        Currency (£ Pound)
       </Dropdown.Toggle>
 
       <Dropdown.Menu className="dropdown">
-        <Dropdown.Item href="#/action-1">£ Pound</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">$ Dollar</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">€ Euro</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">₹ Ruppee</Dropdown.Item>
+        <Dropdown.Item href="#/action-1" eventKey="£">£ Pound</Dropdown.Item>
+        <Dropdown.Item href="#/action-2" eventKey="$">$ Dollar</Dropdown.Item>
+        <Dropdown.Item href="#/action-3" eventKey="€">€ Euro</Dropdown.Item>
+        <Dropdown.Item href="#/action-3" eventKey="₹">₹ Ruppee</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
